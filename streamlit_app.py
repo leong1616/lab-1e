aec33967fdacd69b84365d3ebb04caff9374d796
leong1616 <113882199+leong1616@users.noreply.github.com>
2025-11-11 11:22:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Cheah ")
+st.title("🎈 demo ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
